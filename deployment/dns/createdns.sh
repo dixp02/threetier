1d30c74b858f6335d01508f2cc60246c09084bac
dixp02 <40191633+dixp02@users.noreply.github.com>
2018-10-12 11:47:08 +0530

    Update createdns.sh

--- deployment/dns/createdns.sh
+++ deployment/dns/createdns.sh
@@ -1,4 +1,4 @@
 sleep 30
 LBNAME=`/usr/local/bin/kubectl describe service frontend|grep Ingress|awk '{print $NF}'`
 sed -i "s/FELBNAME/$LBNAME/g" dns.json
-aws route53 change-resource-record-sets --hosted-zone-id Z2RD4DBXZZGW9F --change-batch file://dns.json
+aws route53 change-resource-record-sets --hosted-zone-id Z17MBZKCHE2LTF --change-batch file://dns.json
